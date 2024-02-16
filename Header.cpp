5856005267168b0db4f4912e57d5f6089159a47f
saibhaskarchandhaka <158795719+saibhaskarchandaka@users.noreply.github.com>
2024-02-16 20:20:56 +0530

    Create Header.cpp

---
+++ Header.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   float length,width,height;
+   cout<<"Enter Length,Width and Height : ";
+   cin>>length>>width>>height;
+   boxVolume(length,width,height);
+   boxArea(length,width,height);
+}
+  
