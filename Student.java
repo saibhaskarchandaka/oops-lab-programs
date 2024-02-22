179a450da663ecd2d77e28b08ef701e2539d6116
saibhaskarchandhaka <158795719+saibhaskarchandaka@users.noreply.github.com>
2024-02-22 21:24:09 +0530

    Student.java

--- Student.java
+++ Student.java
@@ -1,64 +1,46 @@
-import java.util.*;
-public class Student
-{   
-    String s_fullName;
-    int s_rollNum;
-    double s_semPercentage;
-    String s_collegeName;
-    int s_collegeCode;
 
-    Student(String fullName,int rollNum,double semPercentage,String collegeName,int collegeCode)
-    {
-        s_fullName=fullName;
-        s_rollNum=rollNum;
-        s_semPercentage=semPercentage;
-        s_collegeName=collegeName;
-        s_collegeCode=collegeCode;
-        System.out.println("Constructor Created");
+import java.util.Scanner;
+
+public class Student {
+    String fullName;
+    int rollNum;
+    double semPercentage;
+    String collegeName;
+    int collegeCode;
+
+    Student(String fullName, int rollNum, double semPercentage, String collegeName, int collegeCode) {
+        this.fullName = fullName;
+        this.rollNum = rollNum;
+        this.semPercentage = semPercentage;
+        this.collegeName = collegeName;
+        this.collegeCode = collegeCode;
     }
-        
-    
-    void display()
-    {
+
+    void display() {
         System.out.println("STUDENT DETAILS");
-        System.out.println("Student name: "+s_fullName);
-        System.out.println(" Roll Number: "+s_rollNum);
-        System.out.println("Sem Percentage: "+s_semPercentage);
-        System.out.println("College Name: "+s_collegeName);
-        System.out.println("College Code: "+s_collegeCode);
+        System.out.println("Student name: " + fullName);
+        System.out.println("Roll Number: " + rollNum);
+        System.out.println("Sem Percentage: " + semPercentage);
+        System.out.println("College Name: " + collegeName);
+        System.out.println("College Code: " + collegeCode);
     }
-    public static void main (String args [] )
-    
-      
-    {
-        String fullName;
-        int rollNum;
-        double semPercentage;
-        String collegeName;
-        int collegeCode;
-        Scanner input=new Scanner(System.in); 
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Student name: ");
-        fullName=input.nextLine();
+        String fullName = input.nextLine();
         System.out.println("Enter your Roll Number: ");
-        rollNum=input.nextInt();
+        int rollNum = input.nextInt();
+        input.nextLine(); 
         System.out.println("Enter your Sem Percentage: ");
-        semPercentage=input.nextDouble();
-        System.out.println("Enter your College Name:  ");
-        collegeName=input.nextLine();
+        double semPercentage = input.nextDouble();
+        input.nextLine(); 
+        System.out.println("Enter your College Name: ");
+        String collegeName = input.nextLine();
         System.out.println("Enter your College Code: ");
-        collegeCode=input.nextInt();
-        Student S1=new Student(fullName,rollNum,semPercentage,collegeName,collegeCode);
-        S1.display();
+        int collegeCode = input.nextInt();
+        Student student = new Student(fullName, rollNum, semPercentage, collegeName, collegeCode);
+        student.display();
         input.close();
     }
-
-    }
-
-
-
-
-
-    
-
-
-
+}
